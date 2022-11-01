3296860bcb1f907e72fd676d8cb7d64fe2e77d8b
Nadezhda <oleneva96@gmail.com>
2022-11-01 15:28:32 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item');
+    const tabsContent = document.querySelectorAll('.tabcontent');
+    const tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabsContent () {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        })
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent (i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+    
+    hideTabsContent ();
+    showTabContent ();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if(target === item) {
+                    hideTabsContent ();
+                    showTabContent (i);
+                }
+            });
+        }
+    })
+
+});
